7338d0197e485b6d69432265feaea98c06fc3f11
Rishav <rishavkapoor38@gmail.com>
2021-10-16 03:18:48 +0530

    added some more feature in thr Memo Book

--- JS/index.js
+++ JS/index.js
@@ -2,20 +2,31 @@ console.log("welcome to the javascript code");
 showNotes();
 // if User adds a notes add it to the local Storage of the Browser
 let addBtn = document.getElementById('addBtn');
+
 addBtn.addEventListener('click', function (e) {
     let addTxt = document.getElementById('addTxt');
+    let addHeading=document.getElementById('addHeading');
     let notes = localStorage.getItem("notes");
-    if (notes == null) {
-        notesArr = [];
+    if(addTxt.value!="" && addHeading.value!=""){
+        if (notes == null) {
+            notesArr = [];
+        }
+        else {
+            notesArr = JSON.parse(notes);
+        }
+        let myObj={
+            heading:addHeading.value,
+            text:addTxt.value
+        }
+        notesArr.push(myObj);
+        localStorage.setItem("notes", JSON.stringify(notesArr));
+        addTxt.value = "";
+        addHeading.value="";
+        // console.log(notesArr);
+        showNotes();
+    }else{
+        return alert("Your Notes is Empty Please Fill the given Fields.");
     }
-    else {
-        notesArr = JSON.parse(notes);
-    }
-    notesArr.push(addTxt.value);
-    localStorage.setItem("notes", JSON.stringify(notesArr));
-    addTxt.value = "";
-    // console.log(notesArr);
-    showNotes();
 })
 function showNotes() {
     let notes = localStorage.getItem("notes");
@@ -27,13 +38,17 @@ function showNotes() {
     }
     let html = "";
     notesArr.forEach(function (element, index) {
+        // console.log(element);
         html += ` <div class="noteCard col-md-4 col-10 col-sm-6">
         <div class="card mx-2 my-2" >
             <div class="card-body">
-                <h3 class="card-title text-center" id="heading">Notes - ${index + 1}</h3>
+                <h3 class="card-title" id="heading">${index+1} ) ${element.heading}</h3>
                 <hr>
-                <p class="card-text" id="para">${element}</p>
-            <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-danger">Delete</button>
+                <p class="card-text" id="para">${element.text}</p>
+                <div class="text-center">
+            <button id="${index}" onclick="deleteNote(this.id)" class="btn"><i class="fas fa-trash-alt"></i></button>
+            <button id="${index}" onclick="editNote(this.id)" class="btn"><i class="far fa-edit"></i></button>
+            </div>
             </div>
         </div>
     </div>`
@@ -49,6 +64,25 @@ function showNotes() {
             </div>
         </div>
     </div>`
+    }
+}
+//function to edit the note.
+function editNote(index){
+    if(addTxt.value ==="" && addHeading.value ===""){
+        let notes = localStorage.getItem("notes");
+        if (notes == null) {
+            notesArr = [];
+        }
+        else {
+            notesArr = JSON.parse(notes);
+        }
+        // console.log(notesArr[index].heading);    
+        addHeading.value=notesArr[index].heading;
+        addTxt.value=notesArr[index].text;
+        deleteNote(index);
+
+    }else{
+        return alert("Please clear the field before Edit.");
     }
 }
 
@@ -75,8 +109,9 @@ searchtxt.addEventListener("input",function(){
     let notesCard=document.getElementsByClassName("noteCard");
     Array.from(notesCard).forEach(function(element){
         let cardTxt=element.getElementsByTagName("p")[0].innerText.toLowerCase();
+        let cardHeading=element.getElementsByTagName("h3")[0].innerText.toLowerCase();
         // console.log(cardTxt);
-        if(cardTxt.includes(inputVal)){
+        if(cardTxt.includes(inputVal) || cardHeading.includes(inputVal)){
             element.style.display="block";
         }else{
             element.style.display="none";
